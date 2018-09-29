68343aafbd269ba4419a2120d9eb5ba8aef48e2b
mike-little <mike-little@users.noreply.github.com>
2018-09-29 16:51:42 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ let cardsAvailable = ['fa fa-diamond',
                       'fa fa-bicycle',
                       'fa fa-bomb'];
 let openCardList = [];
+const starClassName = "fa fa-star";
 
 let restartBtn = document.querySelector('.restart');
 let stars = document.querySelector('.stars');
@@ -18,6 +19,9 @@ let theDeck = document.querySelector('.deck');
 
 let moveCount = 0;
 let matchCount = 0;
+let secondsExpired = 0;
+let theGameTimer = setInterval(gameTimer, 1000);
+let showingFail = false;
 
 /*
  * Go
@@ -32,8 +36,12 @@ play();
  */
 function play() {
     moveCount = 0;
+    showingFail = false;
+    resetTimer();
+    movesCtrl.innerHTML = moveCount + ' moves';
     matchCount = 0;
     resetDeckHtml();
+    updateStars(translateMovesToStars());
     let cards = cardsAvailable.concat(cardsAvailable);
     cards = shuffle(cards);
     for (let i = 0; i < cards.length; i++) {
@@ -46,16 +54,49 @@ function play() {
     }
 }
 
+//
+// Determine how many stars to show based on how many moves the player
+// has taken.
+//
+function translateMovesToStars() {
+    let starCount = 3;
+    if (moveCount > 17 && moveCount < 25 ) {
+        starCount = 2;
+    } else if (moveCount > 25) {
+        starCount = 1;
+    }
+    return starCount;
+}
+
+//
+// Updates the stars displayed to the player
+//
+function updateStars(numStarsToDisplay) {
+    stars.innerHTML = '';
+    for (let i = 0; i < numStarsToDisplay; i++) {
+        stars.innerHTML += '<i class=\"fa fa-star\"></>';
+    }
+}
+
+//
+// Reset the deck of cards for a new game
+//
 function resetDeckHtml() {
     for (let i = (theDeck.children.length -1); i >= 0; i--) {
         theDeck.removeChild(theDeck.children[i]);
     }
 }
 
+//
+// Add event listener for restart button to start a new game
+//
 restartBtn.addEventListener('click', function(event) {
     play();
 });
 
+//
+// Shuffle the deck of cards for each new game
+//
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -71,11 +112,38 @@ function shuffle(array) {
     return array;
 }
 
+//
+// Start timer over for new game
+//
+function resetTimer() {
+    clearInterval(theGameTimer);
+    theGameTimer = setInterval(gameTimer, 1000);
+    secondsExpired = 0;
+    let timerDiv = document.getElementById("timer");
+    timerDiv.innerHTML = secondsExpired + 's';
+}
+
+//
+// Gets called every second while the game is in progress to keep
+// track of how long the player takes to complete the game
+//
+function gameTimer() {
+    secondsExpired++;
+    let timerDiv = document.getElementById("timer");
+    timerDiv.innerHTML = secondsExpired + 's';
+}
+
+//
+// Show this card to the user
+//
 function openCard(card) {
     card.className += ' open show';
     openCardList.push(card.children[0].className);
 }
 
+//
+// Close the cards that are open that do not match
+//
 function clearOpenCardList() {
     if (openCardList.length > 0) {
         for (var i = (openCardList.length - 1); i >= 0; i--) {
@@ -84,6 +152,9 @@ function clearOpenCardList() {
     }
 }
 
+//
+// Lock this card in the open position
+//
 function lockCard(card, otherOpenCardIndex) {
     card.className =  card.className.replace(' open show', ' match');
     for (let i=0; i < theDeck.children.length; i++) {
@@ -93,6 +164,9 @@ function lockCard(card, otherOpenCardIndex) {
     clearOpenCardList();
 }
 
+//
+// Close the cards that are open that do not match
+//
 function closeCards() {
     for (let i=0; i < theDeck.children.length; i++) {
         let cardDetail = theDeck.children[i].className;
@@ -101,14 +175,27 @@ function closeCards() {
     clearOpenCardList();
 }
 
+//
+// Display the number of moves the player has taken so far
+//
 function showMoveCount() {
     moveCount++;
-    movesCtrl.innerHTML = moveCount;
+    movesCtrl.innerHTML = moveCount + ' moves';
+    updateStars(translateMovesToStars());
 }
 
+//
+// Display a winning message to the user
+//
 function showWinner() {
     if (matchCount === 8) {
-        window.alert("Way to go, you won!!! in " + moveCount);
+        if (confirm("Way to go, you won with " + moveCount + " moves in " + 
+                     secondsExpired + " seconds. Rating = " + 
+                     translateMovesToStars() + "\n\t Play again?")) {
+            play();
+        } else {
+            resetTimer();
+        }
     }
 }
 
@@ -123,19 +210,21 @@ function showWinner() {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 theDeck.addEventListener('click', function (event) {
-    if (event.srcElement.className === 'card') {
+    if (event.srcElement.className === 'card' && (showingFail === false)) {
         let card = event.srcElement;
         let cardDetail = card.children[0];
-        showMoveCount();
         if (!(card.className.includes('open show') ||
               card.className.includes('match'))) {
             let openCardIndex = openCardList.indexOf(cardDetail.className);
             openCard(card);
             if (openCardIndex != -1) { // match
+              showMoveCount();
               lockCard(card, openCardIndex);
               matchCount++;
             } else if (openCardList.length > 1) {
-              closeCards();
+              showMoveCount();
+              showingFail = true;
+              setTimeout( function () {closeCards(); showingFail = false;}, 750);
             }
             showWinner();
         }
